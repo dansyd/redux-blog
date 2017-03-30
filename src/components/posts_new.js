42ee83ca62dddb688f6d3d84c6164db6337420f8
Daniele Peviani <daniele.peviani@gmail.com>
2017-03-30 18:10:10 +1100

    Created form and added its reducer

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,35 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
   render() {
+
+    // function passed as props from reduxForm
+    const { handleSubmit } = this.props; // same as handleSubmit = this.props.handleSubmit;
+
     return(
-      <div>Create Form</div>
+      <form onSubmit={handleSubmit}>
+        <h3>Create A New Post</h3>
+        <div className="form-group">
+          <label>Title</label>
+          <input type="text" className="form-contorl"/>
+        </div>
+        <div className="form-group">
+          <label>Categories</label>
+          <input type="text" className="form-contorl"/>
+        </div>
+        <div className="form-group">
+          <label>Title</label>
+          <textarea className="form-contorl"/>
+        </div>
+
+        <button type="submit" className="btn btn-primary">Submit</button>
+      </form>
     );
   }
 }
 
-export default PostsNew;
+export default reduxForm({
+  form: 'PostsNewForm',
+  fields: ['title', 'category', 'content']
+})(PostsNew);
